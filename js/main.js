b7fbd6a0f49752bc38bdcf5b175dd733c283bc8d
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2017-12-15 14:07:40 +0100

    lehen aldaketak

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+$(document).ready(function(){
+
+    $('.divPersona').hide()
+
+    $("input:radio[name='optRemitente']").change(function () {
+
+        if ($("input[name='optRemitente']:checked").val() == '0') {
+            $('.divPersona').show()
+        }
+        if ($("input[name='optRemitente']:checked").val() == '1') {
+            $('.divPersona').hide()
+        }
+    });
+})
